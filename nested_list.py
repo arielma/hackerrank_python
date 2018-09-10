2642918b9f9b1e8d6c0812cb38b088d075bdecdb
Ariel Ma <xiaohang.ma@unimelb.edu.au>
2018-09-11 09:16:30 +1000

    completed nested list

--- nested_list.py
+++ nested_list.py
@@ -4,4 +4,7 @@ if __name__ == '__main__':
         name = input()
         score = float(input())
         score_list.append([name, score])
-    score_list.sort(key = lambda x: (x[1], x[0]))
+    score_set = sorted(set([score for name, score in score_list]))
+    second_lowest_score = score_set[1]
+    second_lowest_name = [name for name, score in score_list if score == second_lowest_score]
+    print("\n".join(sorted(second_lowest_name)))
