299dde52ae1a419c61ee5b5f7cabaa5d3254bf6c
Ariel Ma <ariel.ma822@gmail.com>
2018-09-11 23:07:54 +1000

    added lists.py

---
+++ lists.py
@@ -0,0 +1,21 @@
+if __name__ == '__main__':
+    list = []
+    N = int(input())
+    for i in range(N):
+        command = input().split()
+
+        if command[0] == 'insert':
+            list.insert(int(command[1]), int(command[2]))
+        if command[0] == 'print':
+            print(list)
+        if command[0] == 'remove':
+            list.remove(int(command[1]))
+        if command[0] == 'append':
+            list.append(int(command[1]))
+        if command[0] == 'sort':
+            list.sort()
+        if command[0] == 'pop':
+            list.pop()
+        if command[0] == 'reverse':
+            list.reverse()
+
