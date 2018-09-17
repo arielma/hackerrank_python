1972cbcd2e6fdbd42f50c8f204b2db6efad8b8d2
Ariel Ma <xiaohang.ma@unimelb.edu.au>
2018-09-17 13:58:00 +1000

    added compare_the_triplets

---
+++ compare_the_triplets.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the compareTriplets function below.
+def compareTriplets(a, b):
+    if len(a) == len(b) == 3:
+        score_a = 0
+        score_b = 0
+        for i in range(3):
+            if a[i] > b[i]:
+                score_a += 1
+            if a[i] < b[i]:
+                score_b +=1
+        return [score_a, score_b]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    a = list(map(int, input().rstrip().split()))
+
+    b = list(map(int, input().rstrip().split()))
+
+    result = compareTriplets(a, b)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
