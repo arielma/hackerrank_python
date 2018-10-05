972ab660f41c5489ebfcc1f2d670c02c1d6c9d90
Ariel Ma <xiaohang.ma@unimelb.edu.au>
2018-10-05 11:30:54 +1000

    added code files

---
+++ string_string_validators.py
@@ -0,0 +1,9 @@
+def string_composition(string):
+    print(any(i.isalnum() for i in string))
+    print(any(i.isalpha() for i in string))
+    print(any(i.isdigit() for i in string))
+    print(any(i.islower() for i in string))
+    print(any(i.isupper() for i in string))
+if __name__ == '__main__':
+    s = input()
+    string_composition(s)
